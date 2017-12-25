1d68eba857be4d1a5fc6c0bcf0dbb9ced6bf7c0b
Eric Lin <ericsuhelin@gmail.com>
2017-12-25 15:22:58 -0500

    removed artist from spotify query string, convert unicode dict to string

--- spotifyutil.py
+++ spotifyutil.py
@@ -20,28 +20,44 @@ spotify = spotipy.Spotify(auth=token)
     Takes a year and returns a song object from database
 """
 def getRandomSongByYear(year):
+    randomSong = getRandomSongByYearFromDatabase(year)
+    spotifyRandomSong = getSongFromSpotify(randomSong)
+    return spotifyRandomSong
+
+def getRandomSongByYearFromDatabase(year):
     filename = os.path.join(APP_STATIC, 'output/' + str(year) + '.json')
     yearData = json.load(open(filename))
 
     randomSong = convertUnicodeDictToString(yearData[random.randint(0, RANKING_CUTOFF)])
-    spotifyRandomSong = getSongFromSpotify(randomSong)
-    return spotifyRandomSong
+    return randomSong
 
 def getSongFromSpotify(songData):
     #TODO call spotify endpoint and select track
     queryString = buildSpotifyQuery(songData)
     results = spotify.search(queryString, type='track')
-    track = results[u'tracks']
+    tracks = results['tracks']
+    items = tracks['items']
+
+    for item in items:
+        artists = ""
+        for artist in item['artists']:
+            artists += artist['name']
+        print item['name'] + artists
+    print queryString
+    pprint.pprint(songData)
+
     #TODO check if empty, handle case
-    return track[u'items'][0]
+    # return track[u'items'][0]
 
 def buildSpotifyQuery(songData):
     # TODO create string query from song data using songData.title and songData.artist
     title = songData[u'title'].replace(" ", "+")
-    artist = songData[u'artist'].replace(" ", "+")
-    queryString = title + "+" + artist
+    queryString = title
     return queryString
 
 def convertUnicodeDictToString(dict):
-    # return {k.decode() : v.decode() for k,v in dict.items()}
-    return dict
+    return {str(k) : str(v) for k,v in dict.items()}
+
+def cleanupSongData(songData):
+    # TODO remove "feat." and split artists into an array
+    return
