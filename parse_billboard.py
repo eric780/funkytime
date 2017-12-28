8668dde9d5569c08b963525462dff2d6ec8525e9
Eric Lin <ericsuhelin@gmail.com>
2017-12-28 00:46:45 -0500

    add special case for year 2013 which is funked up

--- parse_billboard.py
+++ parse_billboard.py
@@ -2,14 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import os
+import re
 import unidecode
 import errno
 
 OUTPUT_DIR = '/static/output/'
 
 def main():
-    for year in xrange(2000, 2017):
-        writeYearToJson(year)
+    # for year in xrange(2000, 2017):
+    #     writeYearToJson(year)
+    writeYearToJson(2013)
 
 def writeYearToJson(year):
     assert type(year) is int
@@ -20,16 +22,35 @@ def writeYearToJson(year):
     soup = BeautifulSoup(data, "html.parser") # use default parser
 
     top100 = []
-    mainTableRows = soup.findAll('tr')
-    for mainTableRow in mainTableRows:
-        row = mainTableRow.findAll('td')
-        rank = unidecode.unidecode(row[0].getText())
-        artist = unidecode.unidecode(row[1].getText())
-        title = unidecode.unidecode(row[2].getText())
-        entry = {'rank': rank, 'artist': artist, 'title': title}
-        top100.append(entry)
+    if year == 2013:
+        # handle special case for 2013
+        top100string = soup.findAll('p')[2].getText()
+        top100lines = top100string.split('\n')
+        top100 = list(map(processIndividualLine, top100lines))
+        print top100
+
+    else:
+        mainTableRows = soup.findAll('tr')
+        for mainTableRow in mainTableRows:
+            row = mainTableRow.findAll('td')
+            rank = unidecode.unidecode(row[0].getText())
+            artist = unidecode.unidecode(row[1].getText())
+            title = unidecode.unidecode(row[2].getText())
+            entry = {'rank': rank, 'artist': artist, 'title': title}
+            top100.append(entry)
 
     writeOutput(top100, year)
+
+# Helper for 2013 which for some reason is just one giant string
+def processIndividualLine(line):
+    line = unidecode.unidecode(line)
+    # split by period and dash
+    info = re.split('\.|-', line)
+    rank = info[0].strip()
+    artist = info[1].strip()
+    title = info[2].strip()
+    return {'rank': rank, 'artist': artist,'title': title}
+
 
 def buildURLFromYear(year):
     return "billboardtop100of.com/" + str(year) + "-2/"
